240309ac1742e2034b3c05f6ce6f165bf5153a28
Patrick Mineault <patrick.mineault@gmail.com>
2023-05-02 18:06:03 -0400

    Update post_gen_project.py
    Bump suggested Python version to 3.11

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -26,7 +26,7 @@ If you have not done so already, create a conda environment for your new
 project with:
 
 cd {{cookiecutter.repo_name}}
-conda create --name {{cookiecutter.repo_name}} python=3.8
+conda create --name {{cookiecutter.repo_name}} python=3.11
 conda activate {{cookiecutter.repo_name}}
 conda env export > environment.yml
 
